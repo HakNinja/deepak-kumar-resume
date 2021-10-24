14488ac1dac6ca8aa4ef84e5022dc814dc6435e5
Deepak <hakNinja@gmail.com>
2021-10-25 00:41:16 +0530

    change contact-div

--- index.js
+++ index.js
@@ -177,6 +177,8 @@ function sendEmailfunadmin(email, name, phone,message) {
 
 app.post("/contactus", async(req, res) => {
     const { firstname, lastname, email, contactno, message } = req.body;
+    const temp = await contactschematable.findOne({email:email})
+    if (!temp){
     const contactperson = new contactschematable({
         firstname:firstname,
         lastname: lastname,
@@ -196,6 +198,9 @@ app.post("/contactus", async(req, res) => {
             res.send({ alertmsg: "Thank you for Contacting us...Soon I'll Back to you!" })
         }
     })
+    } else {
+        res.send({ alertmsg: "Already" })
+    }
 })
 
 app.post("/projectqueue", (req, res) => {
